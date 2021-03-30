a2a1c08dd07e9eecfe3e246551d544a9acc52bf2
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-03-30 08:38:36 +0200

    first commit

---
+++ scripts/room2/3_menu.py
@@ -0,0 +1,44 @@
+
+# Lesson A: Appending to Dicts
+"""
+Appending new items to dicts can be done many ways; a common one is the following:
+
+favorite_colors = {}  # {}
+favorite_colors['jim'] = "red"  # {'jim': 'red'}
+favorite_colors['sally'] = "blue"  # {'jim': 'red', 'sally': 'blue'}
+favorite_colors['joe'] = "blue"  # {'jim': 'red', 'sally': 'blue', 'joe': 'blue'}
+"""
+
+# Lesson B: Counting things in Lists and Strings: list.count(), str.count()
+"""
+How many are there in this sequence?  Try the .count() method!
+
+['heads', 'tails', 'heads', 'heads'].count('heads')  # 3
+
+"What a beautiful day".count("a")  # 4
+"""
+
+
+# Exercise:  Ordering Pizza
+"""
+Let's order out for lunch!  
+Have each team member add what kind of pizza they'd like from the menu,
+then make a dictionary containing the number of each menu item you should tell the
+restaurant you all want.
+
+Example:
+orders = ["salami", "salami", "vegetarian"]
+order_counts = {"salami": 2, "vegetarian": 1, "diablo": 0}
+""" 
+
+# Raw Data: 
+menu = ["margherita", "salami", "vegetarian", "hawaiian"]
+orders = []  # One item for each pizza 
+
+# Script (fill in here):
+
+    
+# Output:
+# print(order_counts)
+
+
